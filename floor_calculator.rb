72df231e580f4c967400e295256d7be41acb4714
3rick <erick.dejesus1@alumno.buap.mx>
2023-01-13 21:50:55 -0600

    add: example with select

--- floor_calculator.rb
+++ floor_calculator.rb
@@ -1,8 +1,4 @@
-# text = File.read "input.txt"
-text = "(())"
-puts text
-counter = 0
-stop = text.size - 1
-for i in 0..stop
-    puts i
-end
+text = File.read "input.txt"
+
+out = text.split('').select { |n| n == '(' }
+puts out
